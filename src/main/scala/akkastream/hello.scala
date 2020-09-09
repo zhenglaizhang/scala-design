6a547ea8dd38e9e00e0cef0a4c120fe075eb080a
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2020-09-10 07:58:17 +0800

    hello stream

--- src/main/scala/akkastream/hello.scala
+++ src/main/scala/akkastream/hello.scala
@@ -8,6 +8,7 @@ import akka.util.ByteString
 import scala.concurrent._
 import scala.concurrent.duration._
 import java.nio.file.Paths
+import java.nio.file.Files
 
 object Hello extends App {
   implicit val system = ActorSystem("Hello")
@@ -15,4 +16,12 @@ object Hello extends App {
   val done: Future[Done] = source.runForeach(println)
   implicit val ec = system.dispatcher
   done.onComplete(_ => system.terminate())
+}
+
+object Hello2 extends App {
+  val factorials = Source(1 to 1000).scan(BigInt(1))((acc, next) => acc * next)
+  val result: Future[IOResult] = factorials
+    .throttle(1, 1.second)
+    .map(num => ByteString(s"$num\n"))
+    .runWith(FileIO.toPath(Paths.get("factorials.txt")))
 }
